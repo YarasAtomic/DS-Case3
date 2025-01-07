cb7e98d9304a9cd4e12faec328f63295fbfc03a5
pabloqr <126351137+pabloqr@users.noreply.github.com>
2025-01-07 19:17:04 +0100

    Added sqrt tests

--- master-ugr.calculator.back-end/tests/calculator.backend.test/steps/NumberAttributeSteps.cs
+++ master-ugr.calculator.back-end/tests/calculator.backend.test/steps/NumberAttributeSteps.cs
@@ -35,8 +35,10 @@ namespace calculator.lib.test.steps
                 var jsonDocument = JsonDocument.Parse(responseBody);
                 var odd = jsonDocument.RootElement.GetProperty("odd").GetBoolean();
                 var prime = jsonDocument.RootElement.GetProperty("prime").GetBoolean();
+                var sqrt = jsonDocument.RootElement.GetProperty("sqrt").GetDouble();
                 _scenarioContext.Add("isOdd", odd);
                 _scenarioContext.Add("isPrime", prime);
+                _scenarioContext.Add("sqrt", sqrt);
             }
         }
 
@@ -52,6 +54,13 @@ namespace calculator.lib.test.steps
         {
             var isOdd = _scenarioContext.Get<bool>("isOdd");
             Assert.Equal(isOdd, isIt);
+        }
+
+        [Then(@"the result for its square root is (.*)")]
+        public void ThenTheResultOfItsSqrt(double resSqrt)
+        {
+            var sqrt = _scenarioContext.Get<double>("sqrt");
+            Assert.Equal(sqrt, resSqrt);
         }
     }
 }
