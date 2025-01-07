82ac8b2c0d0c8070f88f5c79c6b9f97f329d14c5
pabloqr <126351137+pabloqr@users.noreply.github.com>
2025-01-07 19:06:58 +0100

    Added Sqrt functionality

--- master-ugr.calculator.back-end/calculator.backend/Controllers/CalculatorController.cs
+++ master-ugr.calculator.back-end/calculator.backend/Controllers/CalculatorController.cs
@@ -47,7 +47,8 @@ namespace CalculatorAPI.Controllers
         {
             var is_prime = NumberAttributter.IsPrime(number);
             var is_odd = NumberAttributter.IsOdd(number);
-            return Ok(new { odd = is_odd, prime = is_prime });
+            var sqrt = NumberAttributter.Sqrt(number);
+            return Ok(new { odd = is_odd, prime = is_prime, sqrt = sqrt });
         }
     }
 }
